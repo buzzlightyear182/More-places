95939ed69fe21609f6cb5a91b8ba56bd90cd12cb
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-12-10 18:54:26 +0800

    Installed Devise and generate Dashboard

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  attr_accessor :login
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,  :uniqueness => {
+    :case_sensitive => false }, presence: true, length: {maximum: 20}, format: {with: /\A[a-zA-Z0-9]+\z/}
+  validates :encrypted_password, presence: true
+  validates :email, presence: true, format: {with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/}, uniqueness: true
+
+end
