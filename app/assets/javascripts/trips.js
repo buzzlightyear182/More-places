6dca080ae3a50e342c30386f414b32dd8189005e
buzzlightyear182 <jane.buzzlightyear@gmail.com>
2014-12-11 23:31:44 +0800

    Cleaner CSS

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -3,14 +3,3 @@
 //     source: $('#trip_country').data('autocomplete-source')
 //   });
 // });
-$(document).ready(function(){
-  $(".dropdown-button").click(function() {
-    $(".dropdown-menu").toggleClass("show-menu");
-    $(".dropdown-menu > li").click(function(){
-      $(".dropdown-menu").removeClass("show-menu");
-    });
-    $(".dropdown-menu.dropdown-select > li").click(function() {
-      $(".dropdown-button").html($(this).html());
-    });
-  });
-});
